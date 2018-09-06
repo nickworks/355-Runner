36a21210ea9e6be0005d34542eb0e2114d5f2c45
Nick <pattison.nick@gmail.com>
2018-09-06 16:43:55 -0400

    Made some track prefabs!
    + Track class
    + Ground material
    + TrackPrefab prefab
    + SceneController.prefabTracks array to hold many different prefabs. It chooses which ones to spawn randomly.
    + SceneController.SpawnSomeTrack()
    - SceneController.prefabWall
    - SceneController.delayUntilSpawn

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
 
-    public GameObject prefabWall;
-    float delayUntilSpawn = 0;
+    public Track[] prefabTracks;
+    List<Track> tracks = new List<Track>();
 
 	void Start () {
-		
+        SpawnSomeTrack();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        delayUntilSpawn -= Time.deltaTime;
-        if (delayUntilSpawn <= 0)
+	void Update () {
+        for(int i = tracks.Count - 1; i >= 0; i--)
         {
-            Vector3 pos = new Vector3(0, 0, 20);
-            Instantiate(prefabWall, pos, Quaternion.identity);
-            delayUntilSpawn = Random.Range(1, 3);
+            if(tracks[i].isDead)
+            {
+                Destroy(tracks[i].gameObject);
+                tracks.RemoveAt(i);
+            }
         }
+
+        if (tracks.Count < 5) SpawnSomeTrack();
+        
 	}
+
+    void SpawnSomeTrack()
+    {
+        while (tracks.Count < 5)
+        {
+
+            Vector3 ptOut = Vector3.zero;
+            if(tracks.Count > 0) ptOut = tracks[tracks.Count - 1].pointOut.position;
+
+
+            Track prefab = prefabTracks[ Random.Range(0, prefabTracks.Length) ]; // random prefab!!!1
+
+
+            Vector3 ptIn = prefab.pointIn.position;
+            Vector3 pos = (prefab.transform.position - ptIn) + ptOut;
+
+            Track newTrack = Instantiate(prefab, pos, Quaternion.identity);
+            tracks.Add(newTrack);
+        }
+    }
+
 }
---
+++ Assets/Track.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour {
+
+    public Transform pointIn;
+    public Transform pointOut;
+
+    public Transform[] wallSpawnPoints;
+
+    public GameObject prefabWall;
+    public float speed = 10;
+
+    [HideInInspector] public bool isDead = false;
+
+    void Start()
+    {
+        if (wallSpawnPoints.Length == 0) return;
+        if (!prefabWall) return;
+
+        // Get a random position:
+        int randIndex = Random.Range(0, wallSpawnPoints.Length);
+        Vector3 spawnPos = wallSpawnPoints[randIndex].position;
+
+        // Spawn a wall, parent it to this chunk of track:
+        Instantiate(prefabWall, spawnPos, Quaternion.identity, transform);
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
+
+        if(pointOut.position.z < -5)
+        {
+            isDead = true;
+        }
+    }
+
+
+}
